740aa2d0a87a1b3e5f18d0a604f605b7bda19dcf
k3ls3yP <kelseypryor1@gmail.com>
2023-06-09 20:08:23 -0500

    grid creation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     private Vector3 targetPos;
+    [SerializeField] private Animator unitAnimator;
+    private GridPosition gridPosition;
 
-    private void Update() {
+
+    private void Awake() {
+        targetPos = transform.position;
+    }
+    private void Start() {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
+    }
+    private void Update() 
+    {
+
 
         float stoppingDistance = .1f;
         Vector3 currentPos = transform.position;
@@ -15,15 +27,29 @@ public class Unit : MonoBehaviour
                 Vector3 moveDirection = (targetPos - transform.position).normalized;
                 float moveSpeed = 4f;  
                 transform.position += moveDirection * moveSpeed * Time.deltaTime;
-            }
 
-        if (Input.GetMouseButtonDown(0))
+                float rotateSpeed = 10f;
+
+                transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+                
+                unitAnimator.SetBool("isWalking", true);
+            }
+        else
         {
-            Move(MouseWorld.GetPosition());
+            unitAnimator.SetBool("isWalking", false);
         }
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition)
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+
+        
     }
 
-    private void Move(Vector3 targetPos)
+    public void Move(Vector3 targetPos)
     {
         this.targetPos = targetPos;
     }
